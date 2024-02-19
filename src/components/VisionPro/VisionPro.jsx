a8cf3982a60921d94dfe4ecde09adeaf4190a3fe
JMartin0823 <juanmartinlopez0823@gmail.com>
2024-02-18 20:24:36 -0500

    overflow x hidden

--- src/components/VisionPro/VisionPro.jsx
+++ src/components/VisionPro/VisionPro.jsx
@@ -11,8 +11,8 @@ export function VisionPro() {
         <img class="logo" src="https://www.apple.com/v/home/bk/images/logos/apple-vision-pro/hero_logo_apple_vision_pro__ea9qv4w4cngy_largetall.png" alt="" />
         <h3 class="visionh3" >Welcome to the era of spatial computing.</h3>
         <div class="link">
-        <a class ="learnmore">Learn More</a>
-        <a class ="buy">Buy</a>
+        <a class ="learnmore">Learn More &gt;</a>
+        <a class ="buy">Buy &gt;</a>
         </div>
         </div>
         </div>
