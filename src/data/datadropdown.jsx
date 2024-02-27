6c9807778081bba0b78dd4a56ef3c6224e7fa88b
JMartin0823 <juanmartinlopez0823@gmail.com>
2024-02-26 22:06:19 -0500

    menu data

---
+++ src/data/datadropdown.jsx
@@ -0,0 +1,14 @@
+export const dataMenu = [
+    {
+        id: 'Shop',
+        items: ['Shop the Latest', 'Mac', 'iPad', 'iPhone', 'Apple Watch', 'Apple Vision Pro', 'Accessories'],
+      },
+      {
+        id: 'Quick Links',
+        items: ['Find a Store', 'Order Status', 'Apple Trade In', 'Financing'],
+      },
+      {
+        id: 'Shop Special Stores',
+        items: ['Certified Refurbished', 'Education', 'Business', 'Veterans and Military', 'Government'],
+      },
+    ];
